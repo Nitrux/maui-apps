c89b18d6841c6070dbbdc64b126a315ab85975a7
Uri Herrera <hello@uriherrera.com>
2021-04-30 02:57:00 -0500

    create pkg

---
+++ travis-build.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+apt-get --yes update
+apt-get --yes install wget equivs curl git
+
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
+git_commit=$(git rev-parse --short HEAD)
+
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: maui-apps" \
+	"Version: 0.0.1-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Architecture: amd64" \
+	"Description: Maui Apps metapackage for Nitrux."
+
+equivs-build configuration
