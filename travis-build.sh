077d6636b27a79699053ea85d61841557f88994d
Uri Herrera <hello@uriherrera.com>
2021-06-08 03:40:18 -0500

    add dep

--- travis-build.sh
+++ travis-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: maui-apps" \
-	"Version: 0.0.4-$git_commit" \
+	"Version: 0.0.5-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: amd64" \
