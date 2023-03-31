651621c844865f93e7ed80c98093408f88a886b0
UriHerrera <hello@uriherrera.com>
2023-03-30 23:31:53 -0600

    Update gh-build.sh

--- gh-build.sh
+++ gh-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: maui-apps-git" \
-	"Version: 0.1.1-$git_commit" \
+	"Version: 0.1.2-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Provides: maui-apps" \
