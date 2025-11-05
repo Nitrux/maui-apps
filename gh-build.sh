24b3526f190bf1655abfa508a3c50952cfa53cf8
Uri Herrera <hello@uriherrera.com>
2025-11-05 06:42:25 -0600

    add deps

--- gh-build.sh
+++ gh-build.sh
@@ -35,7 +35,7 @@ git_commit=$(git rev-parse --short HEAD 2>/dev/null)
 	"Priority: required" \
 	"Homepage: https://nxos.org" \
 	"Package: maui-apps" \
-	"Version: 4.0.2-1-$git_commit" \
+	"Version: 4.0.2-2-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: all" \
